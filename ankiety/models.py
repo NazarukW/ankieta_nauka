2e441bec2b5b165c69333ac4ed2946c31138b42f
nWaldi <ppmip2@gmail.com>
2022-08-15 21:29:59 +0200

    Drugi. dodałem pierwsze pytanie i widok administratora

--- ankiety/models.py
+++ ankiety/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
 class Pytanie(models.Model):
     pytanie_tekst = models.CharField(max_length=200)
     data_publ = models.DateTimeField("data opublikowania ankiety")
+    
+    def __str__(self):
+        return self.pytanie_tekst
+        
+    def ostatnio_opublikowane(self):
+        return self.data_publ >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Wybory(models.Model):
     pytanie = models.ForeignKey(Pytanie, on_delete=models.CASCADE)
     wybory_tekst = models.CharField(max_length=200)
     glosy = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.wybory_tekst
     
