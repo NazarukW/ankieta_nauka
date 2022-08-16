4b419281990e86e2fc6ad662ea8b8cd6552df1eb
nWaldi <ppmip2@gmail.com>
2022-08-16 21:43:27 +0200

    Trzeci. Dodałem templatkę index i zmieniłem view index, detale i głosy

--- ankiety/urls.py
+++ ankiety/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #np /ankiety/
     path("", views.index, name="index"),
+    #np /ankiety/5
+    path("<int:pytanie_id>/", views.detale, name="szczegóły"),
+    #np /ankiety/5/wyniki/
+    path("<int:pytanie_id>/wyniki/", views.wyniki, name="wyniki głosowania"),
+    #np /ankiety/5/glosy
+    path("<int:pytanie_id>/glosy/", views.glosy, name="glosy dla pytania"),
 ]
--- ankiety/views.py
+++ ankiety/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Pytanie
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Witaj świecie. To moja strona z ankietami.")
+    lista_ostatnich_pytan = Pytanie.objects.order_by("data_publ")[:5]
+    context = {
+        "lista_ostatnich_pytan": lista_ostatnich_pytan,
+    }
+    return render(request, "ankiety/index.html", context)
+    
+def detale(request, pytanie_id):
+    return HttpResponse(f"Patrzysz na pytanie nr {pytanie_id}")
+    
+def wyniki(request, pytanie_id):
+    return HttpResponse(f"Patrzysz na wyniki pytania nr {pytanie_id}")
+    
+def glosy(request, pytanie_id):
+    return HttpResponse(f"Patrzysz na głosy pytania nr {pytanie_id}")
+    
