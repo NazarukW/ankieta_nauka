a9dad896708b5e14b90f08c2e42a3e09469fc630
nWaldi <ppmip2@gmail.com>
2022-08-20 22:08:46 +0200

    Dodałęm głosowanie, liczenie głosów i templatki głosowaniem i wynikami

--- ankiety/urls.py
+++ ankiety/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "ankiety"
 urlpatterns = [
     #np /ankiety/
     path("", views.index, name="index"),
     #np /ankiety/5
     path("<int:pytanie_id>/", views.detale, name="szczegóły"),
     #np /ankiety/5/wyniki/
-    path("<int:pytanie_id>/wyniki/", views.wyniki, name="wyniki głosowania"),
+    path("<int:pytanie_id>/wyniki/", views.wyniki, name="wyniki_glosowania"),
     #np /ankiety/5/glosy
-    path("<int:pytanie_id>/glosy/", views.glosy, name="glosy dla pytania"),
+    path("<int:pytanie_id>/glosy/", views.glosy, name="glosy_dla_pytania"),
 ]
--- ankiety/views.py
+++ ankiety/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
-from .models import Pytanie
+from .models import Pytanie, Wybory
 
 # Create your views here.
 
@@ -13,11 +14,25 @@ def index(request):
     return render(request, "ankiety/index.html", context)
     
 def detale(request, pytanie_id):
-    return HttpResponse(f"Patrzysz na pytanie nr {pytanie_id}")
+    pytanie = get_object_or_404(Pytanie, pk=pytanie_id)
+    return render(request, "ankiety/detale.html", {"pytanie": pytanie})
     
 def wyniki(request, pytanie_id):
-    return HttpResponse(f"Patrzysz na wyniki pytania nr {pytanie_id}")
+    pytanie = get_object_or_404(Pytanie, pk=pytanie_id)
+    return render(request, "ankiety/wyniki.html", {"pytanie": pytanie})
     
 def glosy(request, pytanie_id):
-    return HttpResponse(f"Patrzysz na głosy pytania nr {pytanie_id}")
+    pytanie = get_object_or_404(Pytanie, pk=pytanie_id)
+    try:
+        wybrana_odpowiedz = pytanie.wybory_set.get(pk=request.POST["wybory"])
+    except (KeyError, Wybory.DoesNotExist):
+        #ponownie wyświetla formę z pytaniem
+        return render(request, "ankiety/detale.html", {
+            "pytanie": pytanie,
+            "error_message": "Nie wybrałeś żadnej odpowiedzi",
+        })
+    else:
+        wybrana_odpowiedz.glosy += 1
+        wybrana_odpowiedz.save()
+        return HttpResponseRedirect(reverse("ankiety:wyniki_glosowania", args=(pytanie.id,)))
     
